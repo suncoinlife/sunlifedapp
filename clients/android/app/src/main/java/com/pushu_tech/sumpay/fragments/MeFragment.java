5cd5d28c35539d835cce6069c54e6595261110d7
yu.zhao <yu.zhao@puju-tech.com>
2018-03-28 16:29:59 +0800

    add more

--- clients/android/app/src/main/java/com/pushu_tech/sumpay/fragments/MeFragment.java
+++ clients/android/app/src/main/java/com/pushu_tech/sumpay/fragments/MeFragment.java
@@ -1,13 +1,17 @@
 package com.pushu_tech.sumpay.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.pushu_tech.sumpay.R;
+import com.pushu_tech.sumpay.activities.CouponActivity;
+import com.pushu_tech.sumpay.activities.SunCoinActivity;
 
 /**
  * Created by virgil on 09/01/2018.
@@ -15,10 +19,26 @@ import com.pushu_tech.sumpay.R;
 
 public class MeFragment extends Fragment {
 
+    LinearLayout mSuncoinBalanceLayout;
+    LinearLayout mShopGoodsLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, null);
+        mSuncoinBalanceLayout = (LinearLayout) view.findViewById(R.id.suncoinbalance);
+        mShopGoodsLayout = (LinearLayout) view.findViewById(R.id.shopGoods);
+        mSuncoinBalanceLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), SunCoinActivity.class));
+            }
+        });
+        mShopGoodsLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CouponActivity.class));
+            }
+        });
         return view;
     }
 }
--- clients/android/app/src/main/java/com/pushu_tech/sumpay/fragments/ShopFragment.java
+++ clients/android/app/src/main/java/com/pushu_tech/sumpay/fragments/ShopFragment.java
@@ -1,14 +1,20 @@
 package com.pushu_tech.sumpay.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.pushu_tech.sumpay.R;
+import com.pushu_tech.sumpay.activities.ScanActivity;
+import com.pushu_tech.sumpay.activities.ShopActivity;
+import com.pushu_tech.sumpay.views.IconView;
 
 /**
  * Created by virgil on 01/03/2018.
@@ -16,10 +22,44 @@ import com.pushu_tech.sumpay.R;
 
 public class ShopFragment extends Fragment {
 
+    LinearLayout homeshow1;
+    LinearLayout homeshow2;
+    LinearLayout homeshow3;
+    LinearLayout homeshow4;
+    LinearLayout homeshow5;
+    LinearLayout homeshow6;
+    IconView mScanIconView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_shop, null);
+        homeshow1 =(LinearLayout) view.findViewById(R.id.homeshow1);
+        homeshow2 =(LinearLayout) view.findViewById(R.id.homeshow2);
+        homeshow3 =(LinearLayout) view.findViewById(R.id.homeshow3);
+        homeshow4 =(LinearLayout) view.findViewById(R.id.homeshow4);
+        homeshow5 =(LinearLayout) view.findViewById(R.id.homeshow5);
+        homeshow6 =(LinearLayout) view.findViewById(R.id.homeshow6);
+        homeshow1.setOnClickListener(mClickListner);
+        homeshow2.setOnClickListener(mClickListner);
+        homeshow3.setOnClickListener(mClickListner);
+        homeshow4.setOnClickListener(mClickListner);
+        homeshow5.setOnClickListener(mClickListner);
+        homeshow6.setOnClickListener(mClickListner);
+
+        mScanIconView = (IconView)view.findViewById(R.id.scanIconView);
+        mScanIconView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), ScanActivity.class));
+            }
+        });
         return view;
     }
+
+    private View.OnClickListener mClickListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(getContext(), ShopActivity.class));
+        }
+    };
 }
